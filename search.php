5ac14976f1b2b0672d55bb6124360db257649641
daniellobaton <daniellobaton@outlook.com>
2022-06-13 19:28:57 -0500

    Se establece la estructura básica del header de search.php

--- search.php
+++ search.php
@@ -1,5 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultados</title>
 
-echo $_GET['term'];
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 
-?>
+</head>
+<body>
+    
+    <div class="wrapper">
+
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="https://www.google.com.mx/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+
+                    <form action="search.php" method="GET">
+
+                        <div class="searchBarContainer">
+
+                            <input type="text" class="searchBox" name="term">
+
+                            <button>
+
+                                Search
+
+                            </button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
